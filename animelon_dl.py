20bb341714999b00a225c63a47969dbdd0b8aad8
kuuuube <hexagonisalie@gmail.com>
2023-07-09 00:30:13 -0400

    Start of oop removal

--- animelon_dl.py
+++ animelon_dl.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 
-from requests import get, Session
+import requests
 import time
 import os
 import json
-import numpy as np
 import argparse
 import sys
 import subtitle_decryptor
 
 class AnimelonDownloader():
     def __init__(self, sleepTime:int=0, maxTries:int=5, savePath:str="./", subtitlesTypes:list=["englishSub", "romajiSub", "hiraganaSub", "japaneseSub"], sleepTimeRetry=5, qualityPriorities=["ozez", "stz", "tsz"], subtitlesOnly=False):
-        self.baseURL = "https://animelon.com/"
-        self.session = Session()
+        self.session = requests.Session()
         self.headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36" }
-        self.apiVideoFormat = "https://animelon.com/api/languagevideo/findByVideo?videoId=%s&learnerLanguage=en&subs=1&cdnLink=1&viewCounter=1"
         self.session.headers.update(self.headers)
         self.processList = []
         self.sleepTime = sleepTime
@@ -25,211 +22,191 @@ class AnimelonDownloader():
         self.qualityPriorities = qualityPriorities
         self.subtitlesOnly = subtitlesOnly
 
-    def downloadVideo(self, url, fileName=None, stream=None, quality="unknown"):
-        if fileName is None:
-            fileName = url.split("/")[-1] + ".mp4"
-            fileName = os.path.join(self.savePath, fileName)
-        video = stream
-        if video is None:
-            video = self.session.get(url, stream=True)
+def downloadVideo(downloader, url, fileName=None, stream=None, quality="unknown"):
+    if fileName is None:
+        fileName = url.split("/")[-1] + ".mp4"
+        fileName = os.path.join(downloader.savePath, fileName)
+    video = stream
+    if video is None:
+        video = downloader.session.get(url, stream=True)
 
-        block_size = 1024
-        file_size = int(video.headers.get('Content-Length', None))
-        print("Downloading : ", fileName.split('/')[-1], "(%.2f MB)" % (file_size * 1024 ** -2), quality, " quality", " ...")
-        n_chunk = 2
-        self.progressbar(0, file_size, 80, True)
-        with open(fileName, 'wb') as f:
-            for i, chunk in enumerate(video.iter_content(chunk_size = n_chunk * block_size)):
-                f.write(chunk)
-                self.progressbar(i * block_size, file_size, 80, False)
-        return fileName
+    block_size = 1024
+    file_size = int(video.headers.get('Content-Length', None))
+    print("Downloading : " + fileName.split('/')[-1] + " " + "(%.2f MB)" % (file_size * 1024 ** -2) + " " + quality + " quality", " ...")
+    n_chunk = 2
+    progressbar(0, file_size, 80, True)
+    with open(fileName, 'wb') as f:
+        for i, chunk in enumerate(video.iter_content(chunk_size = n_chunk * block_size)):
+            f.write(chunk)
+            progressbar(i * block_size, file_size, 80, False)
+    return fileName
 
-    def progressbar(self, current, max, bar_size, init):
-        if not init:
-            sys.stdout.write("\033[F\033[K\033[F\033[K")
+def progressbar(current, max, bar_size, init):
+    if not init:
+        sys.stdout.write("\033[F\033[K\033[F\033[K")
 
-        percent = str(round(current / max * 100, 2)) + "%"
-        chunks = max / bar_size
-        filled_boxes = int(current / chunks)
-        unfilled_boxes = bar_size - filled_boxes
+    percent = str(round(current / max * 100, 2)) + "%"
+    chunks = max / bar_size
+    filled_boxes = int(current / chunks)
+    unfilled_boxes = bar_size - filled_boxes
 
-        progress_bar = ""
+    progress_bar = ""
 
-        for _ in range(filled_boxes):
-            progress_bar += "█"
+    for _ in range(filled_boxes):
+        progress_bar += "█"
 
-        for _ in range(unfilled_boxes):
-            progress_bar += "░"
+    for _ in range(unfilled_boxes):
+        progress_bar += "░"
 
-        print("%.2f" % (current * 1024 ** -2) + " / " +  "%.2f MB" % (max * 1024 ** -2) + "\n" + progress_bar + " " + percent)
+    print("%.2f" % (current * 1024 ** -2) + " / " +  "%.2f MB" % (max * 1024 ** -2) + "\n" + progress_bar + " " + percent)
 
-    def getSubtitleFromJSON(self, resObj, languageSubList:list=None):
-        if languageSubList is None:
-            languageSubList = self.subtitlesTypes
-        subtitles = []
-        subObj = resObj["subtitles"]
-        for i in subObj:
-            subtitleList = i["content"]
-            for j in languageSubList:
-                if j in subtitleList.keys():
-                    subtitles.append((j, subtitle_decryptor.decrypt_subtitle(subtitleList[j])))
-        return subtitles
+def getSubtitleFromJSON(downloader, resObj, languageSubList:list=None):
+    if languageSubList is None:
+        languageSubList = downloader.subtitlesTypes
+    subtitles = []
+    subObj = resObj["subtitles"]
+    for i in subObj:
+        subtitleList = i["content"]
+        for j in languageSubList:
+            if j in subtitleList.keys():
+                subtitles.append((j, subtitle_decryptor.decrypt_subtitle(subtitleList[j])))
+    return subtitles
 
-    def saveSubtitleToFile(self, languageSub, content, videoName:str="" ,savePath:str=None):
-        if savePath is None:
-            savePath = self.savePath
+def saveSubtitleToFile(downloader, languageSub, content, videoName:str="", savePath:str=None):
+    if savePath is None:
+        savePath = downloader.savePath
 
-        ext = ".ass"
-        if content[0:4] == b"\x31\x0A\x30\x30": #srt magicbytes
-            ext = ".srt"
+    ext = ".ass"
+    if content[0:4] == b"\x31\x0A\x30\x30": #srt magicbytes
+        ext = ".srt"
 
-        iso = {"englishSub" : "en", 'romajiSub' : "ro", "japaneseSub" : "jp", "hiraganaSub" : "hi"}
-        fileName = os.path.join(savePath, videoName + "." + iso[languageSub] + ext)
-        with open(fileName, "wb") as f:
-            f.write(content)
-        return fileName
+    iso = {"englishSub" : "en", 'romajiSub' : "ro", "japaneseSub" : "jp", "hiraganaSub" : "hi"}
+    fileName = os.path.join(savePath, videoName + "." + iso[languageSub] + ext)
+    with open(fileName, "wb") as f:
+        f.write(content)
+    return fileName
 
-    def saveSubtitlesFromResObj(self, resObj, videoName=None, languageSubList:list=None, savePath:str=None):
-        fileNames = []
-        subtitleList = self.getSubtitleFromJSON(resObj, languageSubList)
-        for sub in subtitleList:
-            fileNames.append(self.saveSubtitleToFile(sub[0], sub[1], savePath=savePath, videoName=videoName))
-        return fileNames
-    
-    def downloadFromResObj(self, resObj, fileName=None, saveSubtitle=True, subtitlesOnly=False):
-        title = resObj["title"]
-        if fileName is None:
-            fileName = os.path.join(self.savePath, title + ".mp4")
-        if (saveSubtitle):
-            self.saveSubtitlesFromResObj(resObj, videoName=os.path.basename(fileName).replace(".mp4", ""),
-                savePath=os.path.dirname(fileName))
-        if (self.subtitlesOnly):
-            return "skipped video"
-        video = (resObj["video"])
-        videoURLs = video["videoURLsData"]
-        time.sleep(self.sleepTime)
-        for userAgentKey in videoURLs.keys():
-            #animelon will allow us to download the video only if we send the corresponding user agent
-            #also idk why the userAgent is formatted that way in the JSON, but we have to replace this.
-            self.session.headers.update({"User-Agent": userAgentKey.replace("=+(dot)+=", ".")})
-            mobileUrlList = videoURLs[userAgentKey]
-            videoURLsSublist = mobileUrlList["videoURLs"]
-            for quality in self.qualityPriorities:
-                if quality in videoURLsSublist.keys():
-                    videoURL = videoURLsSublist[quality]
-                    videoStream = self.session.get(videoURL, stream=True)
-                    if videoStream.status_code == 200:
-                        self.downloadVideo(videoURL, fileName=fileName, stream=videoStream, quality=quality)
-                        print ("Finished downloading ", fileName)
-                        return fileName
+def saveSubtitlesFromResObj(downloader, resObj, videoName=None, languageSubList:list=None, savePath:str=None):
+    fileNames = []
+    subtitleList = getSubtitleFromJSON(downloader, resObj, languageSubList)
+    for sub in subtitleList:
+        fileNames.append(saveSubtitleToFile(downloader, sub[0], sub[1], savePath=savePath, videoName=videoName))
+    return fileNames
+
+def downloadFromResObj(downloader, resObj, fileName=None, saveSubtitle=True, subtitlesOnly=False):
+    title = resObj["title"]
+    if fileName is None:
+        fileName = os.path.join(downloader.savePath, title + ".mp4")
+    if (saveSubtitle):
+        saveSubtitlesFromResObj(downloader, resObj, videoName=os.path.basename(fileName).replace(".mp4", ""),
+            savePath=os.path.dirname(fileName))
+    if (downloader.subtitlesOnly):
+        return "skipped video"
+    video = (resObj["video"])
+    videoURLs = video["videoURLsData"]
+    time.sleep(downloader.sleepTime)
+    for userAgentKey in videoURLs.keys():
+        #animelon will allow us to download the video only if we send the corresponding user agent
+        #also idk why the userAgent is formatted that way in the JSON, but we have to replace this.
+        downloader.session.headers.update({"User-Agent": userAgentKey.replace("=+(dot)+=", ".")})
+        mobileUrlList = videoURLs[userAgentKey]
+        videoURLsSublist = mobileUrlList["videoURLs"]
+        for quality in downloader.qualityPriorities:
+            if quality in videoURLsSublist.keys():
+                videoURL = videoURLsSublist[quality]
+                videoStream = downloader.session.get(videoURL, stream=True)
+                if videoStream.status_code == 200:
+                    downloadVideo(downloader, videoURL, fileName=fileName, stream=videoStream, quality=quality)
+                    print ("Finished downloading ", fileName)
+                    return fileName
+    return None
+
+def getEpisodeList(downloader, seriesURL):
+    seriesName = seriesURL.rsplit('/', 1)[-1]
+    url = "https://animelon.com/api/series/" + seriesName
+    statusCode = 403
+    tries = 0
+    while statusCode != 200 and tries < downloader.maxTries:
+        response = downloader.session.get(url)
+        statusCode = response.status_code
+        tries += 1
+        time.sleep(0.5)
+    if (statusCode != 200):
+        print ("Error getting anime info")
         return None
-
-    def downloadFromVideoPage(self, url=None, id=None, fileName=None, saveSubtitle=True):
-        assert(url is not None or id is not None)
-        if url is None:
-            url = self.baseURL + "video/" + id
-        if id is None:
-            id = url.split("/")[-1]
-        
-        apiUrl = self.apiVideoFormat % (id)
-        for tries in range(self.maxTries):
-            response = get(apiUrl, headers=self.headers)
-            if response.status_code == 200:        
-                jsonsed = json.loads(response.content)
-                file = self.downloadFromResObj(jsonsed["resObj"], fileName=fileName, saveSubtitle=saveSubtitle)
-                if file is not None:
-                    return file
-                if file == "skipped video" and self.subtitlesOnly:
-                    return "skipped video"
-                print ("Failed to download ", fileName, "retrying ... (", self.maxTries - tries, " tries left)"),
-                time.sleep(self.sleepTime * tries)
-        print ("Failed to download ", fileName)
+    try:
+        jsoned = json.loads(response.text)
+        resObj = jsoned["resObj"]
+        if resObj is None and '\\' in seriesURL:
+            seriesURL = seriesURL.replace('\\', '')
+            return downloader.getEpisodeList(seriesURL)
+    except Exception as e:
+        print ("Error: Could not parse anime info :\n", e, url , "\n", response, response.content, file=sys.stderr)
         return None
+    return resObj
 
-    def getEpisodeList(self, seriesURL):
-        seriesName = seriesURL.rsplit('/', 1)[-1]
-        url = self.baseURL + "api/series/" + seriesName
-        statusCode = 403
-        tries = 0
-        while statusCode != 200 and tries < self.maxTries:
-            response = self.session.get(url)
-            statusCode = response.status_code
-            tries += 1
-            time.sleep(0.5)
-        if (statusCode != 200):
-            print ("Error getting anime info")
-            return None
+def initSavePath(downloader, name):
+    if downloader.savePath == "./" or name == "":
+        downloader.savePath = name
+    if downloader.savePath == "":
+        downloader.savePath = "./"
+    os.makedirs(downloader.savePath, exist_ok=True)
+    return downloader.savePath
+
+def downloadFromVideoPage(downloader, url=None, id=None, fileName=None, saveSubtitle=True):
+    assert(url is not None or id is not None)
+    if url is None:
+        url = "https://animelon.com/video/" + id
+    if id is None:
+        id = url.split("/")[-1]
+
+    apiUrl = "https://animelon.com/api/languagevideo/findByVideo?videoId=%s&learnerLanguage=en&subs=1&cdnLink=1&viewCounter=1" % (id)
+    for tries in range(downloader.maxTries):
+        response = requests.get(apiUrl, headers=downloader.headers)
+        if response.status_code == 200:
+            jsonsed = json.loads(response.content)
+            file = downloadFromResObj(downloader, jsonsed["resObj"], fileName=fileName, saveSubtitle=saveSubtitle)
+            if file is not None:
+                return file
+            if file == "skipped video" and downloader.subtitlesOnly:
+                return "skipped video"
+            print ("Failed to download ", fileName, "retrying ... (", downloader.maxTries - tries, " tries left)"),
+            time.sleep(downloader.sleepTime * tries)
+    print ("Failed to download ", fileName)
+    return None
+
+def downloadEpisodes(downloader, episodes:dict, title:str, seasonNumber:int=0, savePath:str="./"):
+    index = 0
+    downloadedEpisodes = []
+    for episode in episodes:
+        index += 1
+        url = "https://animelon.com/video/" + episode
+        fileName = title + " S" + str(seasonNumber) + "E" + str(index) + ".mp4"
+        os.makedirs(savePath, exist_ok=True)
+        fileName = os.path.join(savePath, fileName)
+        print(fileName, " : ", url)
         try:
-            jsoned = json.loads(response.text)
-            resObj = jsoned["resObj"]
-            if resObj is None and '\\' in seriesURL:
-                seriesURL = seriesURL.replace('\\', '')
-                return self.getEpisodeList(seriesURL)
+            downloadFromVideoPage(downloader, url, fileName=fileName)
+            downloadedEpisodes.append(index)
         except Exception as e:
-            print ("Error: Could not parse anime info :\n", e, url , "\n", response, response.content, file=sys.stderr)
-            return None
-        return resObj
+            print("Error: Failed to download " + url, file=sys.stderr)
+            print(e)
 
-    def initSavePath(self, name):
-        if self.savePath == "./" or name == "":
-            self.savePath = name
-        if self.savePath == "":
-            self.savePath = "./"
-        os.makedirs(self.savePath, exist_ok=True)
-        return self.savePath
-
-    def downloadEpisodes(self, episodes:dict, title:str, seasonNumber:int=0, savePath:str="./"):
-        index = 0
-        downloadedEpisodes = []
-        for episode in episodes:
-            index += 1
-            url = self.baseURL + "video/" + episode
-            fileName = title + " S" + str(seasonNumber) + "E" + str(index) + ".mp4"
-            os.makedirs(savePath, exist_ok=True)
-            fileName = os.path.join(savePath, fileName)
-            print(fileName, " : ", url)
-            try:
-                self.downloadFromVideoPage(url, fileName=fileName)
-                downloadedEpisodes.append(index)
-            except Exception as e:
-                print("Error: Failed to download " + url, file=sys.stderr)
-                print(e)
-        return downloadedEpisodes
-
-    def downloadSeries(self, url):
-        resObj = self.getEpisodeList(url)
-        if resObj is None:
-            return
-        title = resObj["_id"]
-        print("Title: ", title)
-        seriesSavePath = os.path.join(self.savePath, title)
-        seasons = resObj["seasons"]
-        downloadedEpisodesDict = dict()
-        for season in seasons:
-            seasonNumber = int(season["number"])
-            seasonSavePath = os.path.join(seriesSavePath, "S%.2d" % seasonNumber)
-            os.makedirs(seasonSavePath, exist_ok=True)
-            print("Season %d:" % (seasonNumber))
-            episodes = season["episodes"]
-            downloadedEpisodes = self.downloadEpisodes(episodes, title, seasonNumber=seasonNumber, savePath=seasonSavePath)
-            downloadedEpisodesDict[seasonNumber] = downloadedEpisodes
-        return downloadedEpisodesDict
-
-    def downloadFromURLList(self, urls:list):
-        dlEpisodes = []
-        for url in urls:
-            try:
-                type = url.split('/')[3]
-            except IndexError:
-                print('Error: Bad URL : "%s"' % url)
-                return
-            if type == 'series':
-                self.downloadSeries(url)
-            elif type == 'video':
-                self.downloadFromVideoPage(url)
-            else:
-                print('Error: Unknown URL type "%"' % type, file=sys.stderr)
-        return dlEpisodes
+def downloadSeries(downloader, url, savePath):
+    resObj = downloader.getEpisodeList(url)
+    if resObj is None:
+        return
+    title = resObj["_id"]
+    print("Title: ", title)
+    seriesSavePath = os.path.join(savePath, title)
+    seasons = resObj["seasons"]
+    for season in seasons:
+        seasonNumber = int(season["number"])
+        seasonSavePath = os.path.join(seriesSavePath, "S%.2d" % seasonNumber)
+        os.makedirs(seasonSavePath, exist_ok=True)
+        print("Season %d:" % (seasonNumber))
+        episodes = season["episodes"]
+        downloadEpisodes(downloader, episodes, title, seasonNumber=seasonNumber, savePath=seasonSavePath)
 
 parser = argparse.ArgumentParser(description='Downloads videos from animelon.com')
 parser.add_argument('videoURLs', metavar='videoURLs', type=str, nargs='+', help='A series or video page URL, eg: https://animelon.com/series/Death%%20Note or https://animelon.com/video/579b1be6c13aa2a6b28f1364')
@@ -244,5 +221,18 @@ parser.add_argument('--qualityPriorities', help='Set quality priorities (ozez, s
 args = parser.parse_args()
 
 downloader = AnimelonDownloader(savePath=args.savePath, maxTries=args.maxTries, sleepTime=args.sleepTime, sleepTimeRetry=args.sleepTimeRetry, subtitlesTypes=args.subtitlesType, subtitlesOnly=args.subtitlesOnly, qualityPriorities=args.qualityPriorities)
-downloader.initSavePath(args.savePath)
-downloader.downloadFromURLList(args.videoURLs)
+initSavePath(downloader, args.savePath)
+
+dlEpisodes = []
+for url in args.videoURLs:
+    try:
+        type = url.split('/')[3]
+    except IndexError:
+        print('Error: Bad URL : "%s"' % url)
+        sys.exit()
+    if type == 'series':
+        downloadSeries(downloader, url, args.savePath)
+    elif type == 'video':
+        downloadFromVideoPage(downloader, url)
+    else:
+        print('Error: Unknown URL type "%"' % type, file=sys.stderr)
